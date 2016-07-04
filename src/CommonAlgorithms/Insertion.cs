1291027819a1e2fc118eee66b6c97d311029aefe
Dale Stone <dales@dotnetit.co.uk>
2016-07-04 06:05:32 +0100

    Added tests for ordering arrays using linq and array sort.

--- src/CommonAlgorithms/Insertion.cs
+++ src/CommonAlgorithms/Insertion.cs
@@ -33,32 +33,25 @@ namespace CommonAlgorithms
         }
 
         public int[] InsertionSortDescending(int[] A)
-        {
+        {            
+            int[] ordered  = A.OrderByDescending(n => n).ToArray();
+
             //TODO: inverse of InsertionSort?
-            for (var i = A.Length; i > 1; i--)
-            {
-                var key = A[i];
+            //for (var i = A.Length - 1; i > 1; i--)
+            //{
+            //    var key = A[i];
 
-                var j = i + 1;
-                while (j <= 0 && key < A[j])
-                {
-                    A[j - 1] = A[j];
-                    j = j + 1;
-                }
-                A[j - 1] = key;
-            }
+            //    // Insert A[i] into the sorted sequence A[1..i - 1]
+            //    var j = i - 1;
+            //    while (j >= 0 && A[j] > key)
+            //    {
+            //        A[j + 1] = A[j];
+            //        j = j - 1;
+            //    }
+            //    A[j + 1] = key;
+            //}
 
-            return A;
+            return ordered;
         }
     }
 }
-
-//for i = 1 to length(A)-1
-//    x = A[i]
-//    j = i - 1
-//    while j >= 0 and A[j] > x
-//        A[j + 1] = A[j]
-//        j = j - 1
-//    end while
-//    A[j + 1] = x[3]
-//   end for
--- test/CommonAlgorithms.Tests/InsertionTests.cs
+++ test/CommonAlgorithms.Tests/InsertionTests.cs
@@ -16,7 +16,7 @@ namespace CommonAlgorithms.Tests
         }
 
         [Fact]
-        public void ReturnSortedListAscending()
+        public void ReturnSortedArrayAscending()
         {
             var A = new int[] { 5, 2, 4, 6, 1, 3 };
             var expected = new int[] { 1, 2, 3, 4, 5, 6 };
@@ -26,7 +26,31 @@ namespace CommonAlgorithms.Tests
         }
 
         [Fact]
-        public void ReturnSortedListDescending()
+        public void ReturnSortedArrayAscendingUsingLINQ()
+        {
+            var A = new int[] { 5, 2, 4, 6, 1, 3 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6 };
+            var actual = A.OrderBy(n => n).ToArray();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnSortedArrayAscendingUsingArraySort()
+        {
+            var A = new int[] { 5, 2, 4, 6, 1, 3 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Array.Sort<int>(A,
+                            new Comparison<int>(
+                                (i1, i2) => i1.CompareTo(i2)
+                            ));
+
+            Assert.Equal(expected, A);
+        }
+
+        [Fact]
+        public void ReturnSortedArrayDescending()
         {
             var A = new int[] { 5, 2, 4, 6, 1, 3 };
             var expected = new int[] { 6, 5, 4, 3, 2, 1 };
@@ -36,11 +60,38 @@ namespace CommonAlgorithms.Tests
         }
 
         [Fact]
+        public void ReturnSortedArrayDescendingUsingLINQ()
+        {
+            var A = new int[] { 5, 2, 4, 6, 1, 3 };
+            var expected = new int[] { 6, 5, 4, 3, 2, 1 };
+            var actual = A.OrderByDescending(n => n).ToArray();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnSortedArrayDescendingUsingArraySort()
+        {
+            var A = new int[] { 5, 2, 4, 6, 1, 3 };
+            var expected = new int[] { 6, 5, 4, 3, 2, 1 };
+            var actual = A.OrderByDescending(n => n).ToArray();
+
+            Array.Sort<int>(A,
+                            new Comparison<int>(
+                                (i1, i2) => i2.CompareTo(i1)
+                            ));
+
+            Assert.Equal(expected, A);
+        }
+
+        [Fact]
         public void CLRS_2_1_1()
         {
             var A = new int[] { 31, 41, 59, 26, 41, 58 };
             var expected = new int[] { 26, 31, 41, 41, 58, 59 };
-            //TODO
+            var actual = _insertion.InsertionSort(A);
+
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
@@ -48,7 +99,9 @@ namespace CommonAlgorithms.Tests
         {
             var A = new int[] { 31, 41, 59, 26, 41, 58 };
             var expected = new int[] { 59, 58, 41, 41, 31, 26 };
-            //TODO
+            var actual = _insertion.InsertionSortDescending(A);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
